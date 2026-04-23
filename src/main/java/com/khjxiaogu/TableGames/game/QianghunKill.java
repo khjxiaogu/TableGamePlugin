7174a35ed1590f39cbff56fd6295f9c184fc4cf4
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-04-23 16:24:14 +0800

    more rules

--- src/main/java/com/khjxiaogu/TableGames/game/QianghunKill.java
+++ src/main/java/com/khjxiaogu/TableGames/game/QianghunKill.java
@@ -1,6 +1,7 @@
 package com.khjxiaogu.TableGames.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.khjxiaogu.TableGames.platform.AbstractRoom;
@@ -24,18 +25,51 @@ public class QianghunKill extends Game {
 	@Override
 	public void forceStart() {
 		boolean isQh=false;
+		List<AbstractUser> qhs=new ArrayList<>();
+
+		List<AbstractUser> nqhs=new ArrayList<>();
 		for(AbstractUser a:au) {
 			String memstr=a.getMemberString();
 			String idstr=a.getId().getId();
 			if(memstr.contains("枪魂")||memstr.contains("木仓云鬼")||(idstr.startsWith("2019")&&idstr.endsWith("7996"))||(idstr.startsWith("3256")&&idstr.endsWith("9126"))) {
-				isQh=true;
-				break;
+				qhs.add(a);
+			}else {
+				nqhs.add(a);
 			}
 		}
-		if(isQh)
-			this.sendPublicMessage("游戏结束，枪魂失败！");
-		else
-			this.sendPublicMessage("游戏结束，枪魂胜利！");
+		Collections.shuffle(nqhs);
+		Collections.shuffle(qhs);
+		getScheduler().executeLater(()->{
+			while(true) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					Thread.currentThread().interrupt();
+					break;
+				}
+				if (qhs.isEmpty()&&nqhs.isEmpty()){
+					this.sendPublicMessage("游戏结束，同归于尽！");
+					break;
+				}else if(qhs.isEmpty()) {
+					this.sendPublicMessage("游戏结束，枪魂失败！");
+					break;
+				}else if(nqhs.isEmpty()) {
+					this.sendPublicMessage("游戏结束，枪魂胜利！");
+					break;
+				}
+				AbstractUser qh=qhs.remove(0);
+				AbstractUser nqh=nqhs.remove(0);
+				boolean win=Math.random()>0.3;
+				this.sendPublicMessage(qh+"与"+nqh+"决斗，"+(win?nqh.getMemberString():qh.getMemberString())+"赢了！");
+				if(win)
+					nqhs.add(nqh);
+				else
+					qhs.add(qh);
+					
+			}
+			
+		},1000);
 	}
 
 	@Override
